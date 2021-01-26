c7875b256fa2d24bf4bfa2ea660ddb057ba225dc
Sarah <sarahamann96@gmail.com>
2021-01-25 18:01:47 -0600

    netlify form

--- gatsby-portfolio-dev/src/components/landing/Contact/ContactForm/styles.js
+++ gatsby-portfolio-dev/src/components/landing/Contact/ContactForm/styles.js
@@ -15,4 +15,19 @@ export const Center = styled.div`
 export const InputField = styled.div`
   position: relative;
   margin-bottom: 1rem;
+
+  textarea {
+    width: 100%;
+    min-height: 200px;
+    box-sizing: border-box;
+    border: 2px solid #6c63ff;
+    padding: 0.8rem 1rem;
+    border-radius: 7px;
+    margin-bottom: 0.5rem;
+    transition: 0.3s;
+
+  &::placeholder {
+    color: #a7a7a7;
+  }
+  }
 `;
